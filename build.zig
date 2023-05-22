11f22794941a0c98c22c5557ff38946e46128b74
hizani <nikolay.hizanishvili@gmail.com>
2023-05-22 03:18:12 +0300

    add build script

---
+++ build.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+
+    const days = 25;
+    var paths: [days][18]u8 = undefined;
+    var day_names: [days][5]u8 = undefined;
+    var descriptions: [days][9]u8 = undefined;
+
+    comptime for (paths) |_, idx| {
+        var day = idx + 1;
+        const day_name = std.fmt.comptimePrint("day{d}{d}", .{ day / 10, day % 10 });
+        const path = std.fmt.comptimePrint("src/{s}/main.zig", .{day_name});
+        const description = std.fmt.comptimePrint("Run {s}", .{day_name});
+
+        day_names[idx] = day_name.*;
+        paths[idx] = path.*;
+        descriptions[idx] = description.*;
+    };
+
+    for (paths) |path, idx| {
+        const exe = b.addExecutable(&day_names[idx], &path);
+        exe.setTarget(target);
+        exe.setBuildMode(mode);
+        exe.install();
+
+        const run_cmd = exe.run();
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+        const run_step = b.step(&day_names[idx], &descriptions[idx]);
+        run_step.dependOn(&run_cmd.step);
+    }
+}
