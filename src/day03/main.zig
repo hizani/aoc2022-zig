5db2775a807008c434f7bdef60fde002833998c8
hizani <nikolay.hizanishvili@gmail.com>
2023-05-26 02:50:39 +0300

    make findIntersection more generic

--- src/day03/main.zig
+++ src/day03/main.zig
@@ -1,35 +1,34 @@
 const std = @import("std");
 
-fn charNumber(char: u8) !u8 {
+fn calclulatePriority(char: u8) !u8 {
     return switch (char) {
-        'a'...'z' => char - 'a',
-        'A'...'Z' => char - 'A' + 26,
+        'a'...'z' => char - 'a' + 1,
+        'A'...'Z' => char - 'A' + 27,
         else => error.NotChar,
     };
 }
 
-fn findIntersection(args: anytype) !?u8 {
+fn findIntersection(comptime T: type, args: anytype) ?T {
     const ArgsType = @TypeOf(args);
     const args_type_info = @typeInfo(ArgsType);
 
-    const CHAR_COUNT = 54;
+    const SIZE = std.math.maxInt(T);
     const count_array = switch (args_type_info) {
         .Struct => blk: {
             const fields_info = args_type_info.Struct.fields;
             if (fields_info.len < 2) {
                 @compileError("two or more fields expected, found " ++ fields_info.len);
             }
-            var count_array = [_][CHAR_COUNT]u16{[_]u16{0} ** CHAR_COUNT} ** fields_info.len;
+            var count_array = [_][SIZE]u16{[_]u16{0} ** SIZE} ** fields_info.len;
             inline for (fields_info) |field, i| {
                 const arg_type_info = @typeInfo(field.field_type);
                 if (arg_type_info != .Array) {
                     if (arg_type_info != .Pointer or arg_type_info.Pointer.size != std.builtin.Type.Pointer.Size.Slice) {
-                        @compileError("expected tuple, struct or matrix, found " ++ @typeName(ArgsType));
+                        @compileError("expected tuple, struct or 2d array of iteratable elements, found " ++ @typeName(ArgsType));
                     }
                 }
                 for (@field(args, field.name)) |value| {
-                    const char = try charNumber(value);
-                    count_array[i][char] += 1;
+                    count_array[i][value] += 1;
                 }
             }
             break :blk count_array;
@@ -41,27 +40,26 @@ fn findIntersection(args: anytype) !?u8 {
             const arg_type_info = @typeInfo((@TypeOf(args[0])));
             if (arg_type_info != .Array) {
                 if (arg_type_info != .Pointer or arg_type_info.Pointer.size != std.builtin.Type.Pointer.Size.Slice) {
-                    @compileError("expected tuple, struct or matrix, found " ++ @typeName(ArgsType));
+                    @compileError("expected tuple, struct or 2d array of iteratable elements, found " ++ @typeName(ArgsType));
                 }
             }
 
-            var count_array = [_][CHAR_COUNT]u16{[_]u16{0} ** CHAR_COUNT} ** args.len;
+            var count_array = [_][SIZE]u16{[_]u16{0} ** SIZE} ** args.len;
             inline for (args) |arg, i| {
                 for (arg) |value| {
-                    const char = try charNumber(value);
-                    count_array[i][char] += 1;
+                    count_array[i][value] += 1;
                 }
             }
             break :blk count_array;
         },
-        else => @compileError("expected tuple, struct or matrix, found " ++ @typeName(ArgsType)),
+        else => @compileError("expected tuple, struct or 2d array of iteratable elements, found " ++ @typeName(ArgsType)),
     };
-    comptime var char_idx: u8 = 0;
-    inline while (char_idx < CHAR_COUNT) : (char_idx += 1) {
+    comptime var element_idx: T = 0;
+    inline while (element_idx < SIZE) : (element_idx += 1) {
         var min: u16 = std.math.maxInt(u16);
-        inline for (count_array) |_, set_idx|
-            min = @min(min, count_array[set_idx][char_idx]);
-        if (min > 0) return char_idx + 1;
+        for (count_array) |_, set_idx|
+            min = @min(min, count_array[set_idx][element_idx]);
+        if (min > 0) return element_idx;
     }
     return null;
 }
@@ -81,13 +79,15 @@ pub fn main() !void {
         if (line.len == 0) return error.BadInput;
         //part1
         const mid = line.len / 2;
-        priority1 += try findIntersection(.{ line[0..mid], line[mid..] }) orelse 0;
+        var char = findIntersection(u8, .{ line[0..mid], line[mid..] }) orelse 0;
+        priority1 += try calclulatePriority(char);
         //part2
         std.mem.copy(u8, &elfGroupBuf[group_member], line[0..]);
         elfGroup[group_member] = elfGroupBuf[group_member][0..line.len];
         group_member += 1;
         if (group_member > GROUP_SIZE - 1) {
-            priority2 += try findIntersection(elfGroup) orelse 0;
+            char = findIntersection(u8, elfGroup) orelse 0;
+            priority2 += try calclulatePriority(char);
             group_member = 0;
         }
     }
